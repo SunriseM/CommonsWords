3221edaca43a5885cedeff62f23269fb9048c07d
SunriseM <sunrisem.js@gmail.com>
2018-11-07 17:35:59 -0400

    add api route

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from words import words_count
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+  return "Hello World!"
+
+@app.route("/api/1/get_words", methods=['GET'])
+def get_words():
+  string = request.args.get('string')
+  return jsonify({'words': words_count(string)})
 	
 if __name__ == "__main__":
 	app.run()
