e0311dee02b07f75dfd172c4f64732a15b626b07
SunriseM <sunrisem.js@gmail.com>
2018-11-07 17:36:25 -0400

    add count word function

---
+++ words.py
@@ -0,0 +1,11 @@
+def words_count(string: str) -> dict:
+  words_count = dict()
+  words = string.split(" ")
+
+  for word in words:
+    if word not in words_count:
+      words_count[word] = 0
+    
+    words_count[word] += 1
+  
+  return words_count
