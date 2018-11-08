4010cf2db0629901dc003a5a121e3337531e10cd
SunriseM <sunrisem.js@gmail.com>
2018-11-07 21:09:14 -0400

    use url instead of string

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from words import words_count
+from data import get_content
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,7 +9,9 @@ def hello():
 
 @app.route("/api/1/get_words", methods=['GET'])
 def get_words():
-  string = request.args.get('string')
+  url = request.args.get('url')
+  string = get_content(url)
+
   return jsonify({'words': words_count(string)})
 	
 if __name__ == "__main__":
