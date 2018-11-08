c95859c5bd082b8eee0594014affc55089c026c4
SunriseM <sunrisem.js@gmail.com>
2018-11-08 10:22:11 -0400

    return dicts list instead of dict

--- words.py
+++ words.py
@@ -1,16 +1,25 @@
 import re
 
 def words_count(string: str) -> dict:
-  words_count = dict()
+  words_dict = dict()
+  words_list = list()
 
   ## words = string.split()
 
   words = re.split('[\W_]', string)
 
   for word in words:
-    if word not in words_count:
-      words_count[word] = 0
+    if word not in words_dict:
+      words_dict[word] = 0
     
-    words_count[word] += 1
+    words_dict[word] += 1
+
+  del words_dict[""]
+
+  texts = list(words_dict.keys())
+  frequency = list(words_dict.values())
+
+  for index, text in enumerate(texts):
+    words_list.append({'text': text, 'size': frequency[index]})
   
-  return words_count
+  return words_list
